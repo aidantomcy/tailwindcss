95cf39a166364b6651cefa8e43505c4806888fb3
Robin Malfait <malfait.robin@gmail.com>
2024-03-09 01:24:16 +0100

    ensure the root CSS file part of the `cssImportPaths`

--- packages/@tailwindcss-cli/src/commands/build/index.ts
+++ packages/@tailwindcss-cli/src/commands/build/index.ts
@@ -243,7 +243,9 @@ function handleImports(
   // Relevant specification:
   //   - CSS Import Resolve: https://csstools.github.io/css-import-resolve/
 
-  if (!input.includes('@import')) return [input, []]
+  if (!input.includes('@import')) {
+    return [input, [file]]
+  }
 
   return postcss()
     .use(atImport())
@@ -253,6 +255,8 @@ function handleImports(
 
       // Use `result.messages` to get the imported files. This also includes the
       // current file itself.
-      result.messages.filter((msg) => msg.type === 'postcss-import').map((msg) => msg.file),
+      [file].concat(
+        result.messages.filter((msg) => msg.type === 'dependency').map((msg) => msg.file),
+      ),
     ])
 }
